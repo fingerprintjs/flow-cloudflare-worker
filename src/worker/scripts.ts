1063a4dfabae753e2a1732d5a301f69c27bd3e12
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-09-24 16:21:50 +0200

    chore: add agent script definition

--- src/worker/scripts.ts
+++ src/worker/scripts.ts
@@ -1,7 +1,7 @@
 import { Env } from './types'
 import { getScriptBehaviourPath } from './env'
 
-export type Script = 'injector.iife.js'
+export type Script = 'injector.iife.js' | 'agent.js'
 
 export function getScriptUrl(script: Script, env: Env) {
   return `/${getScriptBehaviourPath(env)}/${script}`
