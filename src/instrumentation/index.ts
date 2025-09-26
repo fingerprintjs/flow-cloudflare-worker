7af0a8c9184f6a4b4e70018c79bdcb939e12e04d
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-09-26 14:39:36 +0200

    refactor: move instrumentation init to a separate file

--- src/instrumentation/index.ts
+++ src/instrumentation/index.ts
@@ -1,22 +1,5 @@
-import { getProtectedApis } from './protectedApis'
-import { patchFetch } from './patcher/fetch'
-import { WritablePatcherContext } from './patcher/context'
-import { setupSignalsCollection } from './signals'
+import { setupInstrumentation } from './instrumentation'
 
-async function main() {
-  const FingerprintJS = window.FingerprintJS
-  if (!FingerprintJS) {
-    console.warn('FingerprintJS is not available, check your worker configuration.')
-    return
-  }
-
-  const patcherCtx = new WritablePatcherContext()
-  const protectedApis = getProtectedApis()
-  patchFetch({ protectedApis, ctx: patcherCtx })
-
-  setupSignalsCollection(patcherCtx)
-}
-
-main().catch((error) => {
+setupInstrumentation().catch((error) => {
   console.error('Error during instrumentation:', error)
 })
---
+++ src/instrumentation/instrumentation.ts
@@ -0,0 +1,18 @@
+import { WritablePatcherContext } from './patcher/context'
+import { getProtectedApis } from './protectedApis'
+import { patchFetch } from './patcher/fetch'
+import { setupSignalsCollection } from './signals'
+
+export async function setupInstrumentation() {
+  const FingerprintJS = window.FingerprintJS
+  if (!FingerprintJS) {
+    console.warn('FingerprintJS is not available, check your worker configuration.')
+    return
+  }
+
+  const patcherCtx = new WritablePatcherContext()
+  const protectedApis = getProtectedApis()
+  patchFetch({ protectedApis, ctx: patcherCtx })
+
+  setupSignalsCollection(patcherCtx)
+}
