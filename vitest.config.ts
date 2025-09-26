d81ca4743ab3e8833889ebf19151b0dd426ea805
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-09-26 14:16:46 +0200

    build: add separate test projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,37 @@
-import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
+import { defineWorkersProject } from '@cloudflare/vitest-pool-workers/config'
+import { defineConfig } from 'vitest/config'
 
-export default defineWorkersConfig({
+export default defineConfig({
   test: {
-    inspector: {
-      port: 3456,
-    },
-    poolOptions: {
-      workers: {
-        wrangler: {
-          configPath: './wrangler.jsonc',
+    projects: [
+      {
+        test: {
+          name: {
+            label: 'Instrumentation',
+            color: 'green',
+          },
+          include: ['__tests__/instrumentation/**/*.test.ts'],
         },
       },
-    },
+      defineWorkersProject({
+        test: {
+          name: {
+            label: 'Worker',
+            color: 'blue',
+          },
+          include: ['__tests__/worker/**/*.test.ts'],
+          inspector: {
+            port: 3456,
+          },
+          poolOptions: {
+            workers: {
+              wrangler: {
+                configPath: './wrangler.jsonc',
+              },
+            },
+          },
+        },
+      }),
+    ],
   },
 })
