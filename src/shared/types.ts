ba77c2fbdd5a8833ea05eeff83f26c12a18269a6
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-09-29 15:21:33 +0200

    chore: remove ruleSetId from ProtectedApi

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -3,7 +3,6 @@ export type ProtectedApiHttpMethod = 'POST' // Only POST is supported for now
 export type ProtectedApi = {
   method: ProtectedApiHttpMethod
   url: string
-  ruleSetId: string
 }
 
 export function isValidHttpMethod(method: string): method is ProtectedApiHttpMethod {
