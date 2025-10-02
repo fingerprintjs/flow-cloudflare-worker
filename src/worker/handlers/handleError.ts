ba36ef0517b686eeeff938716bb6d4afeafcad52
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-10-02 15:26:14 +0200

    feat: support throwing errors without propagating it's message to client

--- src/worker/handlers/handleError.ts
+++ src/worker/handlers/handleError.ts
@@ -4,7 +4,7 @@ export function handleError(error: unknown) {
   console.error('Error handling request', error)
 
   if (error instanceof FlowError) {
-    return new Response(error.message, { status: error.httpStatus })
+    return new Response(error.isPrivate ? '' : error.message, { status: error.httpStatus })
   }
 
   return new Response('Internal Flow Error', { status: 500 })
