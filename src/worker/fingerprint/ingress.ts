ae048b8c69446a14ba685343e1d0b9e1857ad76b
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-10-06 15:10:04 +0200

    docs: mention jira task for V4

--- src/worker/fingerprint/ingress.ts
+++ src/worker/fingerprint/ingress.ts
@@ -6,6 +6,8 @@ import { findCookie } from '../cookies'
 
 /**
  * Request body structure for sending fingerprint data to the ingress service.
+ *
+ * Is in camelCase format until https://fingerprintjs.atlassian.net/browse/PLAT-1437 is resolved
  */
 type SendBody = {
   /** Fingerprint data with signals */
@@ -24,6 +26,8 @@ type SendBody = {
 
 /**
  * Response structure from the ingress service.
+ *
+ * Is in camelCase format until https://fingerprintjs.atlassian.net/browse/PLAT-1437 is resolved. Afterwards we can start using the /v4/send version of the endpoint.
  */
 export type SendResponse = {
   /** Agent data returned by the ingress service */
