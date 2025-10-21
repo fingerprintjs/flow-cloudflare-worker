1fed0ff359145c37fd31e3a42465052e3d61205f
Dan McNulty <daniel.mcnulty@fingerprint.com>
2025-10-21 15:33:26 -0500

    chore: use custom test script for coverage workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'istanbul',
-      reporter: [['text', { file: 'coverage.txt' }], ['lcov'], ['json']],
+      reporter: [['text', { file: 'coverage.txt' }], ['json']],
     },
     projects: [
       {
