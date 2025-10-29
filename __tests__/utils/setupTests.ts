64664fa670e6ac81524c7249367a1d03325730d8
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-10-29 15:33:17 +0100

    test: move vi.mock to top of the file

--- __tests__/utils/setupTests.ts
+++ __tests__/utils/setupTests.ts
@@ -1,12 +1,9 @@
-import { afterEach, beforeEach, vi } from 'vitest'
+import { afterEach, vi } from 'vitest'
 
-beforeEach(() => {
-  // Ensure consistent package.json version for tests
-  vi.mock('../../package.json', async () => ({
-    ...(await vi.importActual('../../package.json')),
-    version: '0.0.1-test',
-  }))
-})
+vi.mock('../../package.json', async () => ({
+  ...(await vi.importActual('../../package.json')),
+  version: '0.0.1-test',
+}))
 
 afterEach(() => {
   vi.clearAllMocks()
