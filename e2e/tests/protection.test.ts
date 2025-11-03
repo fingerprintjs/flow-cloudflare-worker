d709768b3ea32fc8a0490aaaf07d5d25a45797b7
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-11-03 11:38:00 +0100

    test: add more tests for protection

--- e2e/tests/protection.test.ts
+++ e2e/tests/protection.test.ts
@@ -1,11 +1,20 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { getProtectedPath } from '../utils/config'
+import { SIGNALS_KEY } from '../../src/shared/const'
 
 test.describe('Protection', () => {
-  test('should return 403 if signals are missing', async ({ page }) => {
-    await page.goto('/')
+  test('should return empty 403 response if signals are missing', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
+    await page.route(getProtectedPath('/test'), (route, request) => {
+      const headers = {
+        ...request.headers(),
+      }
+      // Delete signals from the request to protected page
+      delete headers[SIGNALS_KEY]
 
-    // On the actual page, send a fetch request to the protected endpoint
+      route.continue({ headers })
+    })
+
     const { status, body } = await page.evaluate(async (url) => {
       const response = await fetch(url, { method: 'POST' })
       return {
@@ -16,5 +25,32 @@ test.describe('Protection', () => {
 
     expect(status).toEqual(403)
     expect(body).toEqual('')
+
+    const protectedRequest = await page
+      .requests()
+      .then((requests) => requests.find((request) => request.url().includes(getProtectedPath('/test'))))
+    expect(protectedRequest).toBeDefined()
+    expect(protectedRequest!.headers()[SIGNALS_KEY]).toBeUndefined()
+  })
+
+  test('should inject signals when protected page is requested', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
+
+    const { status, body } = await page.evaluate(async (url) => {
+      const response = await fetch(url, { method: 'POST' })
+      return {
+        status: response.status,
+        body: await response.text(),
+      }
+    }, getProtectedPath('/test'))
+
+    expect(status).toEqual(403)
+    expect(body).toEqual('')
+
+    const protectedRequest = await page
+      .requests()
+      .then((requests) => requests.find((request) => request.url().includes(getProtectedPath('/test'))))
+    expect(protectedRequest).toBeDefined()
+    expect(protectedRequest!.headers()[SIGNALS_KEY]).toBeTruthy()
   })
 })
