ec0c2b51ba70e0c850aedefc049e501f216b6c39
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-11-03 11:24:14 +0100

    test: add instrumentor tests

---
+++ e2e/tests/instrumentor.test.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test'
+import { getProtectedPath, WORKER_ROUTE_PREFIX } from '../utils/config'
+
+test.describe('Instrumentor script', () => {
+  test('should be injected into specified pages', async ({ page }) => {
+    await page.goto('/')
+
+    const requests = await page.requests()
+    const instrumentorRequest = requests.find((request) => request.url().includes('instrumentor'))
+
+    expect(instrumentorRequest).toBeDefined()
+    expect(instrumentorRequest!.url()).toContain('instrumentor.iife.js')
+
+    const response = await instrumentorRequest!.response()
+    expect(response).toBeTruthy()
+    expect(response!.status()).toBe(200)
+
+    const responseHeaders = response!.headers()
+    expect(responseHeaders['content-type']).toEqual('application/javascript')
+    expect(responseHeaders['cache-control']).toEqual('max-age=60, s-maxage=60')
+
+    const instrumentorCode = await response!.text()
+    expect(instrumentorCode).toContain(WORKER_ROUTE_PREFIX)
+    expect(instrumentorCode).toContain(getProtectedPath('/*'))
+  })
+
+  test('should not be injected into not configured pages', async ({ page }) => {
+    await page.goto('/not-configured')
+
+    const requests = await page.requests()
+    const instrumentorRequest = requests.find((request) => request.url().includes('instrumentor'))
+
+    expect(instrumentorRequest).toBeUndefined()
+  })
+})
