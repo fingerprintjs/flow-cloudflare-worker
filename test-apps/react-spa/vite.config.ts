8a57368244fec2f297c9180637e58104c8a5e96e
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-11-05 12:22:18 +0100

    chore: move worker to react-spa

--- test-apps/react-spa/vite.config.ts
+++ test-apps/react-spa/vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    checker({
+      typescript: true,
+    }),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    cloudflare(),
   ],
   preview: {
     allowedHosts: true,
