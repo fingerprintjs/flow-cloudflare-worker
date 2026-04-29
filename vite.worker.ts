bba6f43f2e86b698e2895afdd553a9e1821511ef
Dan McNulty <daniel.mcnulty@fingerprint.com>
2026-04-29 10:26:34 -0500

    chore: update vite build to produce worker asset with expected filename
    - Update `vite.worker.ts` to copy the produced worker asset to a
      filename that matches the expected asset name in the GitHub release.

--- vite.worker.ts
+++ vite.worker.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import checker from 'vite-plugin-checker'
 import { getLicenseBanner } from './build-utils/license'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
@@ -14,6 +16,18 @@ export default defineConfig({
       },
     }),
     cloudflare(),
+    {
+      // Copy the worker to file with a name that will match the expected asset name in the GitHub release
+      name: 'copy-worker-asset',
+      closeBundle() {
+        const src = resolve(__dirname, 'dist/flow_cloudflare_worker/index.js')
+        const dest = resolve(__dirname, 'dist/flow_cloudflare_worker.js')
+        copyFileSync(src, dest)
+        this.info(
+          'Copying "dist/flow_cloudflare_worker/index.js" to path expected by release workflow "dist/flow_cloudflare_worker.js"'
+        )
+      },
+    } satisfies Plugin,
   ],
   esbuild: {
     banner: getLicenseBanner('Flow Worker'),
